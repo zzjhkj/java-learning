89da671738ef81adafaf768a3706fa6122e80b64
jalufi <168149096+zzjhkj@users.noreply.github.com>
2024-10-21 20:27:29 -0600

    write welcome1

---
+++ chapter2/Welcome1.java
@@ -0,0 +1,7 @@
+
+public class Welcome1{  
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    }// end method main
+}// end class Welcome1
