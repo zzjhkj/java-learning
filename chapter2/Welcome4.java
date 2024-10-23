3202d341c83b023a6344ca7860fb0dc9889c540d
jalufi <168149096+zzjhkj@users.noreply.github.com>
2024-10-22 19:29:41 -0600

    undo the change

--- chapter2/Welcome4.java
+++ chapter2/Welcome4.java
@@ -4,5 +4,5 @@ public class Welcome4 {
     //main method begins execution of Java application 
     public static void main (String[] args) {
         System.out.printf("%s\n%s\n", "Welcome to", "Java Programming");
-    }
-}
+    }// end method main
+}//end class Welcome4
