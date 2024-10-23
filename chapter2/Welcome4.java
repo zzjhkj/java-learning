06f033ed56c0bff2fc8630b7574aa698c2de8764
jalufi <168149096+zzjhkj@users.noreply.github.com>
2024-10-22 19:28:06 -0600

    create Welcome4

---
+++ chapter2/Welcome4.java
@@ -0,0 +1,8 @@
+//Fig.2.6: Welcome4.java
+//displaying multiple lines with method System.out.printf.
+public class Welcome4 {
+    //main method begins execution of Java application 
+    public static void main (String[] args) {
+        System.out.printf("%s\n%s\n", "Welcome to", "Java Programming");
+    }
+}
